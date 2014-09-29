4c9e0df1f64266996ee15543060f2a1a3280accf
Andreas <andreas.weidinger@integrating-architecture.de>
2014-09-29 12:28:16 +0200

    update

--- com.example.ThirdPartyApplication/src/com/example/ThirdPartyApplication.java
+++ com.example.ThirdPartyApplication/src/com/example/ThirdPartyApplication.java
@@ -61,7 +61,7 @@ public class ThirdPartyApplication {
 		//just one more step
 		//and this is a complete client-server application
 		System.out.println(LS+"CONNECTING to Server: "+LS);
-		handler.connect("user", "password",
+		handler.connect("gast", "gast".toCharArray(),
 				"http://integrating-architecture.de", "8080",
 				"isa.esb.ServiceBrokerWebConnector[version=1.0.0.DEV-SNAPSHOT]");
 
