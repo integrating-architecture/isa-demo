fb437e489b3cdf29fd9b000e4a1154333fc68d29
Andreas <andreas.weidinger@integrating-architecture.de>
2014-10-29 10:39:11 +0100

    update

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,9 +1,11 @@
+import java.util.Date;
+
 /*The classic*/
 public class HelloWorld {
 
 	/**
 	 */
 	public String sayHello(String everyBody){
-		return "Hello: "+everyBody;
+		return "Hello: "+everyBody+" - it is: "+new Date();
 	}
 }
